6558c32e9d0c52a9cea3b66293525baf52abcefd
LucasDominicci <lucasdominicci123@gmail.com>
2022-12-17 08:46:03 -0300

    sincronizando

---
+++ app-api/src/database/Usuarios.js
@@ -0,0 +1,19 @@
+//Métodos Get post Put e Delete
+
+//fetch('')
+//.then(data => {
+//    return data.json();
+//    })
+//    .then(post => {
+//    console.log(post.title);
+//    });
+
+
+// Talvez
+/*app.post('' , function(req,res){
+    var  = req.body;
+    //console.log(.codigo);
+    // Adicionar um elemento no array
+    recuperarHtml.push();
+    res.status(201).redirect('');
+});*/
